f5b4dbe68108dffae09b9921c1a167763ebf779c
Ramazan Arıcı <ramazanarc5@gmail.com>
2024-02-22 11:32:10 +0300

    ToDo done func added

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,5 +1,15 @@
 import styles from "./todoitem.module.css";
 export default function TodoItem({ item, todos, setTodos }) {
+  function handleClick(name) {
+    setTodos(
+      todos.map(
+        (todo) => (todo.name === name ? { ...todo, done: !todo.done } : todo)
+        // tıkladıpımız todonun done özelliğini değiştiriyor
+      )
+    );
+  }
+  const doneLine = item.done ? styles.completed : "";
+  // todo tamalandı ise style değişiyor
   function handleDelete(item) {
     console.log("Delete button clicked for item ", item);
     setTodos(todos.filter((todo) => todo !== item));
@@ -10,7 +20,9 @@ export default function TodoItem({ item, todos, setTodos }) {
   return (
     <div className={styles.item}>
       <div className={styles.itemName}>
-        {item}
+        <span className={doneLine} onClick={() => handleClick(item.name)}>
+          {item.name}
+        </span>
         <span>
           <button
             onClick={() => handleDelete(item)}
